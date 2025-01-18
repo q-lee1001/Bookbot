925bb4c63b03030602902d9605edf47108b0a65a
q-lee1001 <quintinlee@live.ca>
2025-01-18 00:30:40 -0800

    C: Added a character counting function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text=get_book_text(book_path)
     word_count=count_words(text)
     print(word_count)
+    char_dictionary=count_characters(text)
+    print(char_dictionary)
 
 def count_words (file_contents):
     words=file_contents.split()
@@ -13,6 +15,15 @@ def get_book_text(path):
     with open(path) as f :
         file_contents = f.read()
         return file_contents
+
+
+def count_characters(text):
+    lowered_text=text.lower()
+    char_count={}
+
+    for char in lowered_text:
+        char_count[char]=char_count.get(char, 0) + 1
+    return char_count
 
 
 main()
